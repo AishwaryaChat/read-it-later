1563fa4d5b327c6bace93c8c84673a2d730795f2
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-22 11:39:58 +0530

    Establish mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const server = express()
 
-server.listen(3000, () => {
-  console.log('listening on port 3000')
+mongoose.connect('mongodb://localhost:27017/tldrdb')
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'db connection error:'))
+db.once('open', (err) => {
+  if (err) {
+    throw new Error('Db not connected')
+  }
+  server.listen(process.env.PORT || 3000)
+  console.log('DB connected successfully and APP listening at: ' + Date())
 })
