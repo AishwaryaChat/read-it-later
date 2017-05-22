855e0cf7c9a0c7b08196ccd9da5edd1316b99aaf
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-22 11:45:14 +0530

    Add sources schema

---
+++ app/models/sources.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// create a schema
+const sourcesSchema = new Schema({
+  id: String,
+  name: String,
+  description: String,
+  url: String,
+  category: String,
+  language: String,
+  country: String,
+  urlsToLogos: {
+    small: String,
+    medium: String,
+    large: String
+  },
+  sortBysAvailable: [],
+  fetchdata: Boolean
+})
+
+// create a mongoose model using schema
+export const Sources = mongoose.model('Sources', sourcesSchema)
