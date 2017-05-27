8db58d8e37d73fe98b6053b9678e1bc2096948cd
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-27 21:24:12 +0530

    Add validation for sign up

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,15 +2,14 @@ const Users = require('../models/users')
 
 // Adding a User
 exports.addUser = (req, res) => {
-  Users.create({
-    name: req.body.first_name + ' ' + req.body.last_name,
-    emailID: req.body.email_id,
-    password: req.body.password
-  }, (err, response) => {
+  Users.findOne({'emailID': req.body.email_id}, (err, doc) => {
+    if (doc) {
+      res.send({message: 'email id already exist'})
+    } else if (!doc) {
+      createUser(req, res)
+    }
     if (err) {
       res.send({err})
-    } else {
-      res.send({message: 'OK'})
     }
   })
 }
@@ -22,6 +21,20 @@ exports.checkUser = (req, res) => {
       res.send({message: 'OK'})
     } else {
       res.send({err})
+    }
+  })
+}
+
+function createUser (req, res) {
+  Users.create({
+    name: req.body.first_name + ' ' + req.body.last_name,
+    emailID: req.body.email_id,
+    password: req.body.password
+  }, (err, response) => {
+    if (err) {
+      res.send({err})
+    } else {
+      res.send({message: 'User successfully registered'})
     }
   })
 }
